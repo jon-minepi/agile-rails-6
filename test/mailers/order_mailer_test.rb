a9e824a517b855786011b247ca143ad12de57dcb
Jon Zivku <jon@minepi.com>
2021-05-17 14:41:43 -0700

    I1 complete

---
+++ test/mailers/order_mailer_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class OrderMailerTest < ActionMailer::TestCase
+  test "received" do
+    order = orders(:one)
+    mail = OrderMailer.received(order)
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal [order.email], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
+  end
+
+  test "shipped" do
+    order = orders(:one)
+
+    mail = OrderMailer.shipped(order)
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal [order.email], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /Programming Ruby 1.9/, mail.body.encoded
+  end
+
+end
